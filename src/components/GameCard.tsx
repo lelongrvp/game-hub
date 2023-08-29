c55569403148b1994c0b50eeacb3993085de070a
Le Duc Long <lelongrvp7@gmail.com>
2023-08-29 11:24:07 +0700

    add platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,7 @@
 import { Game } from "../hooks/useGames"
 import { Card, Image, CardBody, Heading } from "@chakra-ui/react"
+import PlatformIconList from "./PlatformIconList.tsx" 
+
 interface Props { 
   game: Game
 }  
@@ -10,6 +12,7 @@ const GameCard = ({game}: Props) => {
       <Image src={game.background_image}/>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
       </CardBody>
     </Card>
   )
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,34 @@
+import { HStack, Icon } from "@chakra-ui/react" 
+import { Platform } from "../hooks/useGames"
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from "react-icons/fa"
+import { BsNintendoSwitch,  BsGlobe } from "react-icons/bs"
+import { MdPhoneIphone } from "react-icons/md"
+import { IconType } from "react-icons/lib/esm/iconBase"
+
+interface Props {
+  platforms: Platform[]
+}
+
+const PlatformIconList = ({platforms} : Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: BsNintendoSwitch,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid
+  }
+
+  return (
+    <HStack marginY="10px">
+      {platforms.map(platform => (
+        <Icon as={iconMap[platform.slug]} color="gray.500"/>
+      ))}
+    </HStack>
+  )
+}
+
+export default PlatformIconList
