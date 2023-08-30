a4cf09e3371cd2e9dabb8abae5b8b0bbeb595596
Le Duc Long <lelongrvp7@gmail.com>
2023-08-30 11:12:12 +0700

    add genre menu and loading for genre menu

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,23 @@
 import useGenres from "../hooks/useGenres"
-const GenreList = () => {
-  const {data} = useGenres()
+import { List, ListItem, HStack, Image, Text, Spinner } from "@chakra-ui/react"
+import getCroppedImageUrl from "../services/image-url"
 
+const GenreList = () => {
+  const { data, isLoading, error } = useGenres()
+
+  if(error) return null
+  if(isLoading) { return <Spinner/> }
+  
   return (
-    <ul>
-      {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-    </ul>
+    <List>
+      {data.map(genre => ( 
+        <ListItem key={genre.id} paddingY='10px'>
+          <HStack>
+            <Image boxSize='32px' borderRadius='8px' src={getCroppedImageUrl(genre.image_background)}/> 
+            <Text fontSize='lg'>{genre.name}</Text>
+          </HStack>
+        </ListItem>))}
+    </List>
   )
 }
 
